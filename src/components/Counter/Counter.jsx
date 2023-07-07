5a7d094c7744e4928fdece6f0e43d99e6233365c
hemant jayas <hemantjayas03#gmail.com>
2023-07-07 15:26:16 +0530

    useReducer

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,12 +1,51 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { useReducer } from 'react';
 
+const initialState = {
+    counter: 0
+}
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "INCREMENT":
+            return { ...state, counter: state.counter + 1 }
+
+        case "DECREMENT":
+            return { ...state, counter: state.counter - 1 }
+
+        case "RESET":
+            return { ...state, counter: 0 }
+
+
+        default:
+            return state
+            break;
+    }
+}
+
+// useReducer is basically is used to manage the state of the component individually but 
+// redux is used to manage the state of the whole application don't get confused in that. 
 export const Counter = () => {
-    const [count, setCount] = useState(0)
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    const handleIncrement = () => {
+        dispatch({ type: "INCREMENT" })
+    }
+    const handleDecrement = () => {
+        dispatch({ type: "DECREMENT" })
+    }
+    const handleReset = () => {
+        dispatch({ type: "RESET" })
+    }
+
+
     return (
         <div>
-            <h2>Counter: {count}</h2>
-            <button onClick={() => { setCount(count + 1) }}>increment</button>
-            <button onClick={() => { setCount(count - 1) }}>decrement</button>
+            <h2>Counter: {state.counter}</h2>
+            <button onClick={handleIncrement}>increment</button>
+            <button onClick={handleDecrement}>decrement</button>
+            <button onClick={handleReset}>reset</button>
         </div>
     )
 }
+
